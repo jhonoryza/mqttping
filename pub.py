06462107924ed4d75d75c1ee42fd610b00a98345
jhonoryza <jardik.oryza@gmail.com>
2017-09-06 16:47:20 +0700

    jardik

--- pub.py
+++ pub.py
@@ -3,23 +3,24 @@ import time
 from datetime import datetime
 import socket
 
-broker = "103.11.99.171" #"jardik.web.id"
+#broker = "103.11.99.171" 
+broker = "jardik.web.id"
 id = socket.gethostname()
 ip = socket.gethostbyname(socket.gethostname())
+print(id +" " +ip)
+
 client = mqtt.Client(id)
 client.username_pw_set("root", "root")
-client.connect(broker)
+client.connect(broker, 1883, 60)
 client.loop_start()
-
-print(ip)
 
 while True:
 	now = datetime.now()
 	data = now.strftime('%Y/%m/%d %H:%M:%S')
 	#client.publish("opi2g/debug", "message: %s-%s-%s %s:%s:%s"
  #%(now.year, now.month, now.day, now.hour, now.minute, now.second))
-	client.publish(id +"/debug", "{" +id +", "
-			+ip +", "  +data +"}", retain=True)
-	print("pub me \n")
+	message = "{" +id +", " +ip +", "  +data +"}"
+	client.publish(id+"/debug", message, retain=True)
+	print(message +" \n")
 	time.sleep(2)
 		
